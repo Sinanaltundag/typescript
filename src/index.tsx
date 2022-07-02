a0785c4023701a11657d36db7f1d1ca8ad6bbbf1
Sinan Altundağ <87491141+Sinanaltundag@users.noreply.github.com>
2022-07-02 22:12:37 +0300

    add ...redux antdesign router

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from "react-dom/client";
 import App from './App';
+import 'antd/dist/antd.css';
 
 const el = document.getElementById('root')
 if (el === null) throw new Error('Root container missing in index.html')
